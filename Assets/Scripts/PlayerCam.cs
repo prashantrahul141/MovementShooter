78f5ca45fbdef6f5ad1cf797f34258b4b72bfc06
Prashant <prashantrahul141@protonmail.com>
2023-05-22 13:01:35 +0530

    fix horizontal rotation for player orientation

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -28,6 +28,6 @@ public class PlayerCam : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 70f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        playerOrientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        playerOrientation.rotation = Quaternion.Euler(xRotation, yRotation, 0);
     }
 }
