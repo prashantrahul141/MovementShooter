73a8defeda85d51e5f92bbd513310516a4948bac
Prashant <prashantrahul141@protonmail.com>
2023-05-28 16:24:49 +0530

    add throwablesObjects script

--- Assets/Scripts/Player/Throwables/throwableObject.cs
+++ Assets/Scripts/Player/Throwables/throwableObject.cs
@@ -1,3 +1,20 @@
 using UnityEngine;
 
-public class throwableObject : MonoBehaviour { }
+public class throwableObject : MonoBehaviour
+{
+    private Collider throwableCollider;
+
+    private void Start()
+    {
+        throwableCollider = GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer.ToString() != "player")
+        {
+            print("bombed with : " + collision.gameObject.name.ToString());
+            Destroy(gameObject);
+        }
+    }
+}
