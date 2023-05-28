f00def4c5a4c3e9c66c3fa725a43d588bf78a853
Prashant <prashantrahul141@protonmail.com>
2023-05-28 15:28:27 +0530

    add throwables script

---
+++ Assets/Scripts/Player/Throwables/throwablesRig.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class throwablesRig : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private Transform playerCamera;
+
+    [SerializeField]
+    private Transform attackPoint;
+
+    [SerializeField]
+    private GameObject objectToThrow;
+
+    [Header("Stats")]
+    [SerializeField]
+    private int totalThrows;
+
+    [SerializeField]
+    private float throwCoolDown;
+
+    [Header("throwing")]
+    [SerializeField]
+    private KeyCode throwKey = KeyCode.G;
+
+    [SerializeField]
+    private float throwForce;
+
+    [SerializeField]
+    private float throwUpwardForce;
+
+    private bool readyToThrow;
+
+    private void Start()
+    {
+        readyToThrow = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        {
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        GameObject projectile = Instantiate<GameObject>(
+            objectToThrow,
+            attackPoint.position - new Vector3(0f, 1f, 0f),
+            playerCamera.rotation
+        );
+
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceToThrowWith =
+            playerCamera.transform.forward * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToThrowWith, ForceMode.Impulse);
+
+        totalThrows--;
+
+        Invoke(nameof(ResetThrow), throwCoolDown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+}
