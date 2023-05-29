15d308ac4561c20da465c0aad611497af03cb0c3
Prashant <prashantrahul141@protonmail.com>
2023-05-29 23:08:04 +0530

    ad hud canvas script

---
+++ Assets/Scripts/Player/Canvas/HudCanvas.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HudCanvas : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    [SerializeField]
+    private GameObject resumeMenuUI;
+
+    private void Start()
+    {
+        Resume();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        gameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+        resumeMenuUI.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        gameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        resumeMenuUI.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
