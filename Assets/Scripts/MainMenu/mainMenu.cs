c1e7e53b8e0801e275c3d41733d71f2a2669c0d9
Prashant <prashantrahul141@protonmail.com>
2023-06-09 13:05:52 +0530

    add refs to mainmenu_mainmenu and mainmenu_optionsmenu  &  optionsMenu, backtomainmenu, start methods

--- Assets/Scripts/MainMenu/mainMenu.cs
+++ Assets/Scripts/MainMenu/mainMenu.cs
@@ -3,9 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainMenu_mainMenuUI;
+
+    [SerializeField]
+    private GameObject mainMenu_optionsMenuUI;
+
+    public void Start()
+    {
+        mainMenu_mainMenuUI.SetActive(true);
+        mainMenu_optionsMenuUI.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenu_mainMenuUI.SetActive(false);
+        mainMenu_optionsMenuUI.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        mainMenu_optionsMenuUI.SetActive(false);
+        mainMenu_mainMenuUI.SetActive(true);
     }
 
     public void QuitGame()
